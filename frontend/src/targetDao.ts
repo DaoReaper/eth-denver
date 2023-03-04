dea12fd73f8ac9d54926ba6f7cf4d9674bf9fad4
Rowdy <mroddy5280@gmail.com>
2023-03-04 15:59:48 -0700

    Working with single dao

--- frontend/src/targetDao.ts
+++ frontend/src/targetDao.ts
@@ -17,4 +17,9 @@ export const TARGET_DAO: {
     SAFE_ADDRESS: "0xeb0dc703b854791914f30b5a73dd04d8d22a9aff",
     CHAIN_ID: "0x1",
   },
+  "0x3b6b2dd106adbc681f8b846d16b9dbb433bc0ed7": {
+    ADDRESS: "0xf844b98df9ccdfbe5d460d0d7bdca232cf9da923",
+    SAFE_ADDRESS: "0xd9e031ea12e8e9327fcdfc433f0a3a9458f98c7c",
+    CHAIN_ID: "0x5",
+  },
 };
